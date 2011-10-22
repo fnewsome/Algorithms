d5295720bf280cdd35dc3da4f55264cbdfcf7d49
Fanion Newsome <fanionnewsome@gmail.com>
2011-10-22 10:51:56 -0400

    Completed the implementation for the reverse string.

--- Strings/reverse_string.c
+++ Strings/reverse_string.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-
+// Name: swap
+// 
+// Description: Swaps the two given characters
+//
 inline void swap(char *a, char *b){
     *a ^= *b;
     *b ^= *a;
@@ -9,13 +13,30 @@ inline void swap(char *a, char *b){
 }
 
 
+void ReverseString(char *str){
 
+    int i;
+    int N = strlen(str);
+
+
+    for(i=0; i < N/2; ++i){
+        swap(&str[i],&str[N-i-1]);
+    }
+
+}
+
+
+// Name: main
+// 
+// Description: main driver 
+//
 int main(void){
 
     char input[] = "Hello";
 
-    
-
+    printf("Original String: %s\n", input);
+    ReverseString(input); 
+    printf("Reverse String: %s\n", input);
 
     return EXIT_SUCCESS;
 }
