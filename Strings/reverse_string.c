a9670f2097e06928380d2ad1deaac5f15a2345e4
Fanion Newsome <fanionnewsome@gmail.com>
2011-10-22 10:36:39 -0400

    Initial check-in for the reverse string function

---
+++ Strings/reverse_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+inline void swap(char *a, char *b){
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+
+
+
+int main(void){
+
+    char input[] = "Hello";
+
+    
+
+
+    return EXIT_SUCCESS;
+}
