623f05286722a1be1bd13d8519ab5dfabf3d6995
Fanion Newsome <fanion.newsome@dodadr.com>
2011-10-28 08:44:56 -0400

    began SLL template

---
+++ DataStructures/LinkedList/Template/CustomSLL.h
@@ -0,0 +1,17 @@
+#include "Node.h"
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class CustomSLL{
+    
+
+
+    private:
+        Node<T> *head, *tail;   
+        int _size;
+        
+
+};
+
---
+++ DataStructures/LinkedList/Template/Node.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class Node{
+    private:
+        T item;
+        Node *next;
+
+};
+
