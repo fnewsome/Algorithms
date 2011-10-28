6a8675b9d29be6b6a226cfa51e5bc3c43e1b1c78
Fanion Newsome <fanionnewsome@gmail.com>
2011-10-28 19:08:56 -0400

    changed filename of SLL class

---
+++ DataStructures/LinkedList/Template/SLL.h
@@ -0,0 +1,19 @@
+#include "Node.h"
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+class SLL{
+
+    public:
+    
+        
+
+    private:
+        Node<T> *head, *tail;   
+        int _size;
+        
+
+};
+
